eba27be26917c0bb94bdbae1975bd02c9b1b868d
Tomi <tomi.ambro94@gmail.com>
2020-09-09 20:45:33 -0300

    Initial commit

---
+++ arduino/commands.sh
@@ -0,0 +1,12 @@
+echo "[info] Starting"
+# echo "[info] Compiling sketch: "
+# arduino-cli compile --fqbn arduino:avr:uno ./Test
+arduino-cli board list
+sleep 5
+echo "[info] Uploadling sketch: "
+arduino-cli upload -p /dev/ttyACM0 --verify --fqbn arduino:avr:uno ./Test --verbose &> output.txt
+while true; do
+	tail -n 4 output.txt
+	echo "I'm alive"
+	sleep 15
+done
