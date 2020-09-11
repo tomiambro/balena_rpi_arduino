59a465690e22875781c7c005b3eda4da26d8d02c
Tomi <tomi.ambro94@gmail.com>
2020-09-11 00:24:27 -0300

    Removed comments and sleep commands

--- arduino/commands.sh
+++ arduino/commands.sh
@@ -1,8 +1,6 @@
 echo "[info] Starting"
-# echo "[info] Compiling sketch: "
-# arduino-cli compile --fqbn arduino:avr:uno ./Test
+
 arduino-cli board list
-sleep 5
 echo "[info] Uploadling sketch: "
 arduino-cli upload -p /dev/ttyACM0 --verify --fqbn arduino:avr:uno ./Test --verbose &> output.txt
 while true; do
